b225c6abc4c0de6f5f8ca05cd4e3fd71289652df
micah <1569124799@qq.com>
2023-12-13 23:11:11 +0800

    更改图标

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -231,43 +231,43 @@ export default hopeTheme({
       cachePic: true,
       appendBase: true,
       apple: {
-        icon: "/assets/icon/apple-icon-152.png",
+        icon: "/assets/icon/144.png",
         statusBarColor: "black",
       },
       msTile: {
-        image: "/assets/icon/ms-icon-144.png",
+        image: "/assets/icon/144.png",
         color: "#ffffff",
       },
       manifest: {
         icons: [
           {
-            src: "/assets/icon/chrome-mask-512.png",
+            src: "/assets/icon/512.png",
             sizes: "512x512",
             purpose: "maskable",
             type: "image/png",
           },
           {
-            src: "/assets/icon/chrome-mask-192.png",
+            src: "/assets/icon/192.png",
             sizes: "192x192",
             purpose: "maskable",
             type: "image/png",
           },
           {
-            src: "/assets/icon/chrome-512.png",
+            src: "/assets/icon/512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/assets/icon/chrome-192.png",
+            src: "/assets/icon/192.png",
             sizes: "192x192",
             type: "image/png",
           },
         ],
         shortcuts: [
           {
-            name: "Demo",
-            short_name: "Demo",
-            url: "/start/",
+            name: "Korepi Start",
+            short_name: "Korepi Start",
+            url: "/en/start/",
             icons: [
               {
                 src: "/assets/icon/guide-maskable.png",
